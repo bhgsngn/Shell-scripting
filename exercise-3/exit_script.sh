6edcf498e8533618c52667c2d2878dbc883fba7f
Bhagas Nguyen <bhagaswara01@gmail.com>
2021-12-15 16:43:29 +0100

    exercise-3

---
+++ exercise-3/exit_script.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+echo Hi ! Let us imagine something goes wrong and exit with a return code of 5
+exit 5
+
---
+++ exercise-3/ls_ordered_with_arguments.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+echo The name of the program is $0
+echo The first argument is $1
+echo The second argument is $2
+echo The third argument is $3
+echo The fourth argument is $4.
